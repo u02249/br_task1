1e7bad3d8559ae3d1672b8608607235aa0a87e2c
u02249 <u02249@gmail.com>
2019-05-01 10:37:12 +0600

    added readme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-restricted-globals */
-// eslint-disable-next-line no-undef
-const doc = document;
+// eslint-disable-next-line no-nnundef
+const doc = document; // для того чтобы линтер не ругался на то что переменная не определена
+// получим все элементы на странице
 const output = doc.getElementById('output');
 const button = doc.getElementById('btnSum');
 const firstInput = doc.getElementById('firstInput');
@@ -8,16 +9,33 @@ const secondInput = doc.getElementById('secontInput');
 firstInput.value = 0.1;
 secondInput.value = 0.2;
 
+// функция приводит строкове представление числа к нужному форматы
+// обрезает 0
+function trimZero(num) {
+  const removeLastZero = chars => {
+    const lastChar = chars[chars.length - 1];
+    if (lastChar !== '0') {
+      return chars;
+    }
+    chars.pop();
+    return removeLastZero(chars);
+  }
+  const chars = [...num.toString()]
+  return removeLastZero(chars).join('');
+}
+// выводит сообщение в блок output 
 function print(message) {
   const item = doc.createElement('p');
   item.innerHTML = message;
   output.appendChild(item);
 }
 
+// отчищает выведенные ранее сообщения
 function clearOutput() {
   output.innerHTML = '';
 }
 
+// событие обработки клика по кнопке "сложить"
 button.addEventListener('click', () => {
   const a = Number(firstInput.value);
   const b = Number(secondInput.value);
@@ -31,8 +49,10 @@ button.addEventListener('click', () => {
     print('Не верно введен номер во второе поле!');
     isValid = false;
   }
+  // если значения введены корректно
+  // делаем расчет суммы, обрезаем до "безопасного" значения и приводим нужному формату
   if (isValid) {
-    const res = (a * 100000000000000000 + b * 100000000000000000) / 100000000000000000;
-    print(res);
+    const res = (a + b).toFixed(16);
+    print(trimZero(res));
   }
 })
